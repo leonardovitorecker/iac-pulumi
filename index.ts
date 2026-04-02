eaa47a171050e8a5f83566a25cda9ed94b49ca45
Leonardo Vitor Ecker <72422376+leonardovitorecker@users.noreply.github.com>
2026-04-01 21:36:27 -0300

    pulumi

--- index.ts
+++ index.ts
@@ -3,12 +3,22 @@ import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
 // Create an AWS resource (S3 Bucket)
-export const bucket = new aws.s3.Bucket("widget-server-stg", {
-  bucket: "widget-server-stg",
+export const firstBucket = new aws.s3.Bucket("first-bucket-widget-server-stg", {
+  bucket: "first-bucket-widget-server-stg",
   tags: {
     IAC: "true",
   },
 });
+
+export const secondBucket = new aws.s3.Bucket(
+  "second-bucket-widget-server-stg",
+  {
+    bucket: "second-bucket-widget-server-stg",
+    tags: {
+      IAC: "true",
+    },
+  },
+);
 
 const ecr = new aws.ecr.Repository("segundo-teste-post-rocketseat", {
   name: "segundo-teste-pos-rocketseat",
@@ -18,6 +28,13 @@ const ecr = new aws.ecr.Repository("segundo-teste-post-rocketseat", {
   },
 });
 
-export const bucketName = bucket.id;
-export const bucketInfo = bucket.bucket;
-export const bucketArn = bucket.arn;
+export const firstBucketName = firstBucket.id;
+export const firstBucketInfo = firstBucket.bucket;
+export const firstBucketArn = firstBucket.arn;
+
+export const secondBucketName = secondBucket.id;
+export const secondBucketInfo = secondBucket.bucket;
+export const secondBucketArn = secondBucket.arn;
+
+export const ecrName = ecr.name;
+export const ecrRepositoryUrl = ecr.repositoryUrl;
