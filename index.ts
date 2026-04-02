2155b72b5eecf7e51081b7e9e57b9c8f34dce553
Leonardo Vitor Ecker <72422376+leonardovitorecker@users.noreply.github.com>
2026-04-01 22:06:31 -0300

    s3 pulumi

--- index.ts
+++ index.ts
@@ -10,6 +10,16 @@ export const firstBucket = new aws.s3.Bucket("first-bucket-widget-server-stg", {
   },
 });
 
+export const secondBucket = new aws.s3.Bucket(
+  "second-bucket-widget-server-stg",
+  {
+    bucket: "second-bucket-widget-server-stg",
+    tags: {
+      IAC: "true",
+    },
+  },
+);
+
 const ecr = new aws.ecr.Repository("segundo-teste-post-rocketseat", {
   name: "segundo-teste-pos-rocketseat",
   imageTagMutability: "IMMUTABLE",
@@ -21,6 +31,10 @@ const ecr = new aws.ecr.Repository("segundo-teste-post-rocketseat", {
 export const firstBucketName = firstBucket.id;
 export const firstBucketInfo = firstBucket.bucket;
 export const firstBucketArn = firstBucket.arn;
+
+export const secondBucketName = secondBucket.id;
+export const secondBucketInfo = secondBucket.bucket;
+export const secondBucketArn = secondBucket.arn;
 
 export const ecrName = ecr.name;
 export const ecrRepositoryUrl = ecr.repositoryUrl;
